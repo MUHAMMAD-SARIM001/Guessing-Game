96887e2f7cae68e640894eb368427b2ffe04e526
MUHAMMAD-SARIM <muhammad.sarim001100@gmail.com>
2024-10-23 04:15:15 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random());
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number b/w 1-10:",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("Congratulatuions!You Guessed Right Number.");
+}
+else {
+    console.log("you Guessed wrong Number");
+}
